e73fc08b4f6ef6e3e51b2ff69581ccd2f1c58969
alex <alexhwang01234@gmail.com>
2021-09-22 19:26:20 -0700

    13.  you updated the cart reducer ealier

--- src/components/PortfolioSummary/SummaryOfPortfolio.js
+++ src/components/PortfolioSummary/SummaryOfPortfolio.js
@@ -5,7 +5,7 @@ const SummaryOfPortfolio = () => {
     <section className={classes.summary}>
       <h2>This your portfolio</h2>
       <p>
-        You are on 142. Using the Context
+        You are on 143. Adding a Cart Reducer
       </p>
       
     </section>
