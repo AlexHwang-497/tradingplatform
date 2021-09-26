cd13f25489f70c04f5c5ef5209ec7e041154a366
alex <alexhwang01234@gmail.com>
2021-09-26 14:54:54 -0700

    20.  included order from, checkout.js

--- src/components/OrderHistory/OrderHistory.js
+++ src/components/OrderHistory/OrderHistory.js
@@ -1,13 +1,15 @@
-import {useContext} from 'react'
+import {useContext, useState} from 'react'
 
 import Modal from '../UI/Modal'
 import OrderHistoryCartItem from './OrderHistoryCartItem'
 import classes from '../OrderHistory/OrderHistory.module.css'
 import CartContext from '../store/CartContext'
+import Checkout from './Checkout'
 
 // ! this is the equivalent of Cart.js
 
 const OrderHistory = (props) => {
+    const [isCheckout,setIsCheckout] = useState(false)
     const cartCtx = useContext(CartContext)
     console.log('1.cartCtx: ' + JSON.stringify(cartCtx.items))
     const totalAmount=`$${cartCtx.totalAmount.toFixed(2)}`
@@ -19,6 +21,10 @@ const OrderHistory = (props) => {
     const cartItemAddHandler = (item) => {
         cartCtx.addItem({...item, amount:1})
     }
+
+    const orderHandler =() =>{
+        setIsCheckout(true)
+    }
 // ! look in cartcontext's item.  understand what is being pulled in the item
 
     const orders = (
@@ -27,13 +33,25 @@ const OrderHistory = (props) => {
                 <OrderHistoryCartItem
                     key={item.id}
                     name ={item.symbol}
-                    amount={item.amount}
-                    price={item.current_price}
+                    amount={item.market}
+                    price={item.profit}
                     onRemove={cartItemRemoveHandler.bind(null,item.id)}
                     onAdd={cartItemAddHandler.bind(null,item)}
                 />
             ))}
         </ul>
+    )
+    const modalActions = (
+        <div className={classes.actions}>
+            <button className={classes['button--alt']} onClick = {props.onClose}>
+                Close
+            </button>
+            {hasItems && (
+                <button className={classes.button} onClick={orderHandler}>
+                    Order
+                </button>
+            )}
+        </div>
     )
 
     return(
@@ -43,12 +61,8 @@ const OrderHistory = (props) => {
                 <span>Total Amount</span>
                 <span>{totalAmount}</span>
             </div>
-            <div className={classes.actions}> 
-                <button className={classes['button--alt']} onClick={props.onClose}>
-                    Close
-                </button>
-                {hasItems && <button className={classes.button}>Order</button>}
-            </div>         
+                {isCheckout && <Checkout onCancel={props.onClose}/>}
+                {!isCheckout && modalActions}
         </Modal>
     )
 }
